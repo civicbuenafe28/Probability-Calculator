4b8a5e1786751fc76292963edb6fa2bcfcd6f4a5
Carl Victor A. Buenafe <152045070+civicbuenafe28@users.noreply.github.com>
2025-06-04 00:25:48 +0800

    Add files via upload

---
+++ ProbabilityDistribution/src/Measurements/Pos.java
@@ -0,0 +1,64 @@
+package Measurements;
+
+public class Pos {
+
+    public static int MAIN_PANEL_X = 0;
+    public static int MAIN_PANEL_Y = 0;
+
+    public static int WELCOME_PANEL_X = 0;
+    public static int WELCOME_PANEL_Y = 0;
+
+    public static int TITLE_X = 135;
+    public static int TITLE_Y = 30;
+
+    public static int LOGO_WHITE_X = 495 - 100;
+    public static int LOGO_WHITE_Y = 301;
+
+    public static int LOGO_X = 35;
+    public static int LOGO_Y = 19;
+
+    public static int OUTCOME_PANEL_X = 491;
+    public static int OUTCOME_PANEL_Y = 152;
+
+    public static int NUM_OF_OUTCOMES_X = 76;
+    public static int NUM_OF_OUTCOMES_Y = 135;
+
+    public static int OUTCOMES_PROB_X = 76;
+    public static int OUTCOMES_PROB_Y = 282;
+
+    public static int CONTROL_PANEL_X = 76;
+    public static int CONTROL_PANEL_Y = 530;
+
+    private static int DISTRIBUTION_X = 491;
+
+    public static int MEAN_OUTCOME_X = DISTRIBUTION_X;
+    public static int MEAN_OUTCOME_Y = 513;
+
+    public static int VAR_OUTCOME_X = DISTRIBUTION_X;
+    public static int VAR_OUTCOME_Y = 587;
+
+    public static int SD_OUTCOME_X = DISTRIBUTION_X;
+    public static int SD_OUTCOME_Y = 661;
+
+    public static int CALC_BUTTON_X = 96;
+    public static int CALC_BUTTON_Y = 565;
+
+    public static int UNDO_BUTTON_X = 270;
+    public static int UNDO_BUTTON_Y = 565;
+
+    public static int CLEAR_BUTTON_X = 96;
+    public static int CLEAR_BUTTON_Y = 633;
+
+    public static int EXIT_BUTTON_X = 270;
+    public static int EXIT_BUTTON_Y = 633;
+
+    public static int SET_BUTTON_X = 329;
+    public static int SET_BUTTON_Y = 220;
+
+    public static int ADD_BUTTON_X = 329;
+    public static int ADD_BUTTON_Y = 426;
+
+    public static int OUTCOMES_AREA_X = 516;
+    public static int OUTCOMES_AREA_Y = 233;
+
+}
---
+++ ProbabilityDistribution/src/Measurements/Scale.java
@@ -0,0 +1,46 @@
+package Measurements;
+
+public class Scale {
+
+    public static int WINDOW_W = 1024;
+    public static int WINDOW_H = 752 + 50;
+
+    public static int MAIN_PANEL_W = WINDOW_W;
+    public static int MAIN_PANEL_H = WINDOW_H - 30;
+
+    public static int WELCOME_PANEL_W = WINDOW_W;
+    public static int WELCOME_PANEL_H = WINDOW_H - 30;
+
+    public static int TITLE_W = 348;
+    public static int TITLE_H = 58;
+
+    public static int LOGO_WHITE_W = 290 - 100;
+    public static int LOGO_WHITE_H = 290 - 100;
+
+    public static int LOGO_W = 81;
+    public static int LOGO_H = 81;
+
+    public static int OUTCOME_PANEL_W = 457;
+    public static int OUTCOME_PANEL_H = 316;
+
+    public static int NUM_OF_OUTCOMES_W = 372;
+    public static int NUM_OF_OUTCOMES_H = 126;
+
+    public static int OUTCOMES_PROB_W = 372;
+    public static int OUTCOMES_PROB_H = 189;
+
+    public static int CONTROL_PANEL_W = 372;
+    public static int CONTROL_PANEL_H = 172;
+
+    public static int DISTRIBUTION_W = 458;
+    public static int DISTRIBUTION_H = 58;
+
+    public static int CONTROL_BUTTON_W = 157;
+    public static int CONTROL_BUTTON_H = 34;
+
+    public static int SET_ADD_W = 101;
+    public static int SET_ADD_H = 35;
+
+    public static int OUTCOMES_AREA_W = 406;
+    public static int OUTCOMES_AREA_H = 208;
+}
